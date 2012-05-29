d8fb0742d3cfa7cae40011997caae642e44af8e0
Anirudh S <gingerjoos@gmail.com>
2012-05-29 11:41:34 +0530

    js version

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+function fetch_pirate_tweets() {
+	var username = $('#id-username').val();
+	var tweetContainer = $('#tweet-wrap');
+	fetch_tweets_and_translate(username,tweetContainer);
+}
+
+function fetch_tweets_and_translate(username,tweetContainer) {
+	var url = 'https://api.twitter.com/1/statuses/user_timeline.json?screen_name=' + username + '&include_rts=true&callback=?';
+	$.getJSON(url,function(data) {
+		var out = '<ol>';
+		$.each(data,function(i,item) {
+			out = out + '<li>' + item.text + '</li>';
+		});
+		out = out + '</ol>';
+		tweetContainer.html(out);
+		var translated = translate_to_ps(out,tweetContainer);
+	});
+}
+
+function translate_to_ps(tweets,tweetContainer) {
+	var api_url = 'http://query.yahooapis.com/v1/public/yql?callback=?';
+	var query = 'SELECT * FROM piratespeak.translate WHERE html = "' + encodeURIComponent(tweets) + '"';
+	$.getJSON(api_url,
+			{
+			'format':'json',
+			'env':'store://kid666.com/piratespeak',
+			'q':query,
+			},
+			function(data) {
+				var result = data.query.results.result;
+				tweetContainer.html(result);
+			});
+}
